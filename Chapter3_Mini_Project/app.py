2e5ba6def12edf9685b644bd91b4e47d1d4d43a1
unknown <pthonganh11092001@gmail.com>
2026-04-21 10:42:24 +0700

    Implement add book and formatted display

--- Chapter3_Mini_Project/app.py
+++ Chapter3_Mini_Project/app.py
@@ -27,12 +27,69 @@ def save_data(filename, books):
             f.write(f"{b['id']}|{b['title']}|{b['author']}|{b['year']}|{b['quantity']}|{b['category']}|{b['status']}\n")
 
 
+def add_book(books):
+    print("--- ADD BOOK ---")
+
+    book_id = input("ID: ")
+    title = input("Title: ")
+    author = input("Author: ")
+
+    while True:
+        try:
+            year = int(input("Year: "))
+            break
+        except:
+            print("Year must be a number!")
+
+    while True:
+        try:
+            quantity = int(input("Quantity: "))
+            break
+        except:
+            print("Quantity must be a number!")
+
+    category = input("Category: ")
+    status = input("Status (Available/Borrowed): ")
+
+    books.append({
+        "id": book_id,
+        "title": title,
+        "author": author,
+        "year": year,
+        "quantity": quantity,
+        "category": category,
+        "status": status
+    })
+
+    print("Added successfully!")
+
+def display_books(books):
+    if not books:
+        print("No books found.")
+        return
+
+    print("\n{:<6} {:<15} {:<15} {:<8} {:<6} {:<12} {:<10}".format(
+        "ID", "Title", "Author", "Year", "Qty", "Category", "Status"
+    ))
+    print("-" * 75)
+
+    for b in books:
+        print("{:<6} {:<15} {:<15} {:<8} {:<6} {:<12} {:<10}".format(
+            b["id"],
+            b["title"],
+            b["author"],
+            b["year"],
+            b["quantity"],
+            b["category"],
+            b["status"]
+        ))
+
+
 def display_menu():
-    print("\n===== FAMILY LIBRARY MANAGEMENT =====")
+    print("===== FAMILY LIBRARY MANAGEMENT =====")
     print("1. Add book")
     print("2. Display books")
     print("0. Exit")
-
 
 def main():
     file = "Chapter3_Mini_Project/books.txt"
@@ -43,10 +100,10 @@ def main():
         choice = input("Choose: ")
 
         if choice == "1":
-            print("Chưa làm add")
+            add_book(books)
 
         elif choice == "2":
-            print(books)
+            display_books(books)
 
         elif choice == "0":
             save_data(file, books)
@@ -59,3 +116,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
